8ff6ea3c5e0899c87a6dbfe182301fc081c80f64
nileshprasad137 <prasadnilesh96@gmail.com>
2017-11-08 17:19:27 +0530

    [Lesson] VERY important while considering hasOne

--- app/Thread.php
+++ app/Thread.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Post;
+use App\User;
 
 class Thread extends Model
 {
@@ -15,5 +16,13 @@ class Thread extends Model
     public function post_one()
     {
         return $this->hasOne(Post::class);
+    }
+    public function threadLeader()
+    {
+        //return $this->hasOne(User::class);
+        /**
+         * We can't use hasOne Relationship here as there is no column called thread_id in users table.
+         * We were able to use hasOne relationship in posts() function as we had thread_id column in posts table
+         */
     }    
 }
