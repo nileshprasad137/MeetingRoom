312ee8277b22298e0c24f485ee15d7c5bc2a0f26
nileshprasad137 <prasadnilesh96@gmail.com>
2017-11-09 18:55:13 +0530

    minor ui change

--- resources/views/threads.blade.php
+++ resources/views/threads.blade.php
@@ -5,7 +5,7 @@
   <h2>Recent Discussions</h2>
   <p>Login to start discussions.</p>                                                                                      
   <div class="table-responsive">          
-  <table class="table">
+  <table class="table table-hover">
     <thead>
       <tr>
         <th>Title</th>
