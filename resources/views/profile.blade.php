c6917e6969e5d1e66ebb987347ab8ddc4e198560
nileshprasad137 <prasadnilesh96@gmail.com>
2017-11-23 20:33:03 +0530

    [new feature] user can upload profile pics

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <img src="/uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
+            <img src="uploads/avatars/{{ $user->avatar }}" style="width:150px; height:150px; float:left; border-radius:50%; margin-right:25px;">
             <h2>{{ $user->name }}'s Profile</h2>
             <form enctype="multipart/form-data" action="{{route('avatar.update')}}" method="POST">
                 <label>Update Profile Image</label>
